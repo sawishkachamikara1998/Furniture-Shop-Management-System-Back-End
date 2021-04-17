6e0a54445cf1ea5b7231b8f0a42c14d20c896fad
sawishkachamikara1998 <75958619+sawishkachamikara1998@users.noreply.github.com>
2021-04-17 07:26:44 +0530

    branch update

--- src/modules/branch/branch.routes.js
+++ src/modules/branch/branch.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
 
+import * as branchController from "./branch.controller";
+
 const routes = new Router();
+
+routes.post("/create", branchController.createBranch);
+
+routes.get("/getAll", branchController.getAllBranches);
 
 export default routes;
